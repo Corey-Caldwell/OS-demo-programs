bdbcf7b3a08da89f073d99778e32d0e49151fb6c
Bryan Clair <bryan@slu.edu>
2015-09-17 23:31:52 -0500

    Changed to handle up to 64 bit values.

--- threads/prime.C
+++ threads/prime.C
@@ -4,25 +4,31 @@
   Multithreaded prime number seeker.
   Find a bunch of prime numbers.
 
+  usage: prime primesize numthreads
+         primesize - bits in each integer
+
   Bryan Clair
-  Sep. 2013
+  2013-2015
 */
 
 #include <iostream>
 #include <stdlib.h>
 #include <pthread.h>
 #include <math.h>
+#include <stdint.h>
+
 using namespace std;
 
-int howmany;  // how many primes are left to find.
+int primesize;  // global holds the # of bits in each integer.
 
 //
 // isprime: return True if n is prime
+//          Note use of int64_t for a 64-bit integer.
 //
-bool isprime(long n)
+bool isprime(int64_t n)
 {
-  long i,s;
-  s = (long) sqrt((double) n);
+  int64_t i,s;
+  s = (int64_t) sqrt((double) n) + 1;
 
   for (i = 2; i <= s; i++)
     if (n % i == 0) {
@@ -33,17 +39,23 @@ bool isprime(long n)
 }
 
 //
+// random_bits : return a random n bit integer (64>n>32)
+//
+int64_t random_bits(int n) {
+  return (((int64_t)random()) << n-32) ^ random();
+}
+
+//
 // seek_primes
 //    Thread function to look for primes
 //
 void *seek_primes(void *unused)
 {
-  long test;
-  while (howmany > 0) {
-    test = random();
+  int64_t test;
+  while (true) {
+    test = random_bits(primesize);
     if (isprime(test)) {
       cout << test << " is prime!" << endl;
-      howmany--;
     }
   }
 }
@@ -52,7 +64,8 @@ void *seek_primes(void *unused)
 // usage - print a usage statement and exit
 //
 void usage(const char *message) {
-  cerr << "usage: prime numthreads howmany" << endl;
+  cerr << "usage: prime primesize numthreads" << endl;
+  cerr << "             primesize - bits in each integer" << endl;
   if (message) cerr << "       " << message << endl;
   exit(1);
 }
@@ -66,10 +79,11 @@ int main(int argc, char *argv[])
   // Check command line arguments
   if (argc != 3) usage(NULL);
 
-  int num_threads = atoi(argv[1]);
+  primesize = atoi(argv[1]);
+
+  int num_threads = atoi(argv[2]);
   if (num_threads <= 0) usage("numthreads must be > 0");
-    
-  howmany = atoi(argv[2]);
+  
 
   // Create threads
   pthread_t thread_id;
